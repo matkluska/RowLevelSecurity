37ca32da333dfb92b4ee8435458e4de4f1c31dac
Mateusz Kluska <kluska.mat@gmail.com>
2017-01-21 19:34:18 +0100

    Small refactor

--- Company/Company.cs
+++ Company/Company.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Data.Entity;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.SqlClient;
-using RowLevelSecurity.Context;
 
 namespace RowLevelSecurity.Company
 {
@@ -10,37 +7,41 @@ namespace RowLevelSecurity.Company
     {
         public static void PrintMenu(CompanyContext c)
         {
-            Console.WriteLine("Zaloguj się do systemu: ");
-            Console.WriteLine("1. Boss");
-            Console.WriteLine("2. Accountant");
-            Console.WriteLine("3. Programist");
-            Console.WriteLine("4. Intern");
-            Console.WriteLine("0. Exit");
-            var res = Console.ReadLine();
-            switch (res)
+            while (true)
             {
-                case "1":
-                    c.SetUsername("Boss_");
-                    break;
-                case "2":
-                    c.SetUsername("Accountant_");
-                    break;
-                case "3":
-                    c.SetUsername("Programist_");
-                    break;
-                case "4":
-                    c.SetUsername("Intern_");
-                    break;
-                case "0":
-                    System.Environment.Exit(1); ;
-                    break;
-                default:
-                    Console.Clear();
-                    Console.WriteLine("Niepoprawny wybór");
-                    PrintMenu(c);
-                    break;
-            }
+                Console.WriteLine("Zaloguj się do systemu: ");
+                Console.WriteLine("1. Boss");
+                Console.WriteLine("2. Accountant");
+                Console.WriteLine("3. Programist");
+                Console.WriteLine("4. Intern");
+                Console.WriteLine("0. Exit");
+                var res = Console.ReadLine();
+                switch (res)
+                {
+                    case "1":
+                        c.SetUsername("Boss_");
+                        break;
+                    case "2":
+                        c.SetUsername("Accountant_");
+                        break;
+                    case "3":
+                        c.SetUsername("Programist_");
+                        break;
+                    case "4":
+                        c.SetUsername("Intern_");
+                        break;
+                    case "0":
+                        Environment.Exit(1);
+                        ;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Niepoprawny wybór");
+                        continue;
+                }
 
+                break;
+            }
         }
 
         public static void PrintEmployeeEarnings()
@@ -51,7 +52,7 @@ namespace RowLevelSecurity.Company
 
                 PrintMenu(context);
 
-                
+
                 foreach (var contextEmp in context.Employees)
                 {
                     //printing employees table
@@ -69,19 +70,18 @@ namespace RowLevelSecurity.Company
                         contextEmp.Surname,
                         contextEmp.Role);
                     Console.WriteLine("------------------------------------------------");
-                    
+
                     //printing earnings table
                     Console.WriteLine(
-                            "{0,-10} {1,-10} {2,-4} {3,-5} {4,-3} {5,-10}",
-                            "EmployeeId",
-                            "Value",
-                            "Year",
-                            "Month",
-                            "Day",
-                            "EarningId");
+                        "{0,-10} {1,-10} {2,-4} {3,-5} {4,-3} {5,-10}",
+                        "EmployeeId",
+                        "Value",
+                        "Year",
+                        "Month",
+                        "Day",
+                        "EarningId");
                     Console.WriteLine("------------------------------------------------");
                     foreach (var e in contextEmp.EarningsList)
-                    { 
                         Console.WriteLine(
                             "{0,-10} {1,-10} {2,-4} {3,-5} {4,-3} {5,-10}",
                             e.EmployeeRefId,
@@ -90,17 +90,15 @@ namespace RowLevelSecurity.Company
                             e.Date.Month,
                             e.Date.Day,
                             e.EarningId);
-                    }
                     Console.WriteLine();
                 }
             }
         }
+
         public static void Main(string[] args)
         {
             while (true)
-            {
                 PrintEmployeeEarnings();
-            }
         }
     }
 }
